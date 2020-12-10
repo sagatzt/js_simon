290dd28f46f4f53d670f94da27d73b0491d76c00
Jose Luis Bch <sagatzt@gmail.com>
2020-12-10 08:06:39 +0100

    first commit

--- js/juego.js
+++ js/juego.js
@@ -1,37 +1,11 @@
 let notas=document.querySelectorAll(".audio")
-let modal=window.parent.document.querySelector(".modal")
-let alert=window.parent.document.querySelector(".alert")
 let simon=document.querySelectorAll(".color")
 let comprobar=document.querySelector("#btnComprobar")
 let empezar=document.querySelector("#btnEmpezar")
-let partida=JSON.parse(sessionStorage.getItem("simon"))
-let jugadaUser=[]
 
-//inicializar la partida:
-document.querySelector("body").onload=()=>{
-    if(!partida) console.log("no hay partidas")
-}
-//boton empezar
-empezar.onclick=()=>{
-    if(partida.vidas>0){
-        partida.jugada.push(Math.round(Math.random()*3))
-        lanzarJugada(partida.jugada)
-        jugadaUser=[]
-    }else
-        console.log("No tiene vidas para jugar")
-}
 
-document.querySelector(".simon").onclick=(e)=>{
-    let id=Number(e.target.dataset.id)
-    encenderColor(id)
-    jugadaUser.push(id)
-    if(jugadaUser.length==partida.jugada.length){
-        console.log(jugadaUser)
-        console.log(partida.jugada)
-    }
-}
 
-//sistema interactivo de luz y sonido de cuadritos
+//inicia secuencia colores cuadritos
 const lanzarJugada=(jugada)=>{
     let cuadrito=0
     let interval=setInterval(()=>{
@@ -40,7 +14,7 @@ const lanzarJugada=(jugada)=>{
         if(cuadrito==jugada.length) clearInterval(interval)
     },800)   
 }
-//enciende cuadrito 1 segundo
+//enciende cuadrito .6 segundos
 const encenderColor=(cuadrito)=>{
     notas[4].play()
     simon[cuadrito].classList.toggle("color-off")
